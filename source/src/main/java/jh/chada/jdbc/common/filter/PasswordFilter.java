d8ffdd7ce91a054755c15c93093274351422a4b8
shin4sjh <shinsharejh@gmail.com>
2023-08-01 23:16:18 +0900

    CHADA PROJECT
    프로젝트 생성
    dynamic web project
    jsp - css,js,jpg,svg,ico,png 지원하기 위한 servlet-mapping
    filter (한글, 암호화)

---
+++ source/src/main/java/jh/chada/jdbc/common/filter/PasswordFilter.java
@@ -0,0 +1,23 @@
+package jh.chada.jdbc.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import jh.chada.jdbc.common.filter.wrapper.PasswordWrapper;
+
+public class PasswordFilter implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		PasswordWrapper pwrapper = new PasswordWrapper(httpRequest);
+		chain.doFilter(pwrapper, response);  // request 대신 wrapper 거친 값을 넣어줌
+	}
+}
